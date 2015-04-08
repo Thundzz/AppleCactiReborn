fe9470b40f4262151be5790e1a97eb5ec8acfcfb
Guillaume Rincel <guillaume.rincel@inria.fr>
2015-04-08 13:55:18 +0200

    defined joker trap

--- src/game/game.c
+++ src/game/game.c
@@ -24,51 +24,12 @@ int game_over (){
   return 1;
 }
 
-
-int game_use_joker(joker_t * j){
-  move_t move;
-  position_t *position;
-  switch(j->type)
-    {
-    case JOKER_CHARGE:
-      move.direction = j->joker.charge.direction;
-      game_play_move(&move);
-      game_play_move(&move);
-      //no break, following code common to charge and procrastination
-    case JOKER_PROCRASTINATION:
-      player_next();
-      break;
-    case JOKER_CORRUPTION:
-      position = &j->joker.corruption.position;
-      // check the position is valid. An externally developped IA might 
-      // try to corrupt something else than an opponent pawn.
-      if(board.board[position->i][position->j] == current_opponent_pawn())
-	board.board[position->i][position->j] = current_player_pawn();
-      else
-	{
-	  fputs(INVALID_MOVE, stderr);
-	  // the current player made an invalid move (likely a bug). His opponent wins.
-	  //TODO
-	  ;
-	}
-      break;
-    case JOKER_STEAMROLLER:
-      break;
-    case JOKER_TRAP:
-      break;
-    case JOKER_REINFORCEMENT:
-      break;
-    default:
-      fputs(ERROR_YOUDONWANNANO, stderr);
-      break;
-    }
-  return 0;
-}
-
-/* used to determine when to remove a column/line (if there's no pawn on it) */
-int tile_not_empty(tile_content_t t) 
+void invalid_move()
 {
-  return t == APPLE || t == CACTUS;
+  fputs(INVALID_MOVE, stderr);
+  // the current player made an invalid move (likely a bug). His opponent wins.
+  //TODO
+  
 }
 
 /* Empty lines/columns on the edges of the board fall apart */
@@ -91,6 +52,61 @@ void destroy_empty_edges()
 	    board.board[i][j] = VOID;
 	}
     }
+}
+
+int game_use_joker(joker_t * j){
+  move_t move;
+  position_t *position;
+  switch(j->type)
+    {
+    case JOKER_CHARGE:
+      move.direction = j->joker.charge.direction;
+      game_play_move(&move);
+      game_play_move(&move);
+      //no break, following code common to charge and procrastination
+    case JOKER_PROCRASTINATION:
+      player_next();
+      break;
+    case JOKER_CORRUPTION:
+      position = &j->joker.corruption.position;
+      // check the position is valid. An externally developped IA might 
+      // try to corrupt something else than an opponent pawn.
+      if(board.board[position->i][position->j] == current_opponent_pawn())
+	board.board[position->i][position->j] = current_player_pawn();
+      ; //animation : transformation of the pawn
+      else
+	invalid_move();
+      break;
+    case JOKER_STEAMROLLER:
+      for(int i = 0; i < BOARD_SIZE; ++i)
+	{
+	  ; // animation : a steamroller crosses row j of the board
+	  if(board.board[i][j->joker.steamroller.j] != VOID)
+	    board.board[i][j->joker.steamroller.j] = EMPTY;
+	}
+      //this might result in an empty row/column on the edges
+      destroy_empty_edges();
+      break;
+    case JOKER_TRAP:
+      position = &j->joker.trap.position;
+      if(board.board[position->i][position->j] != EMPTY)
+	invalid_move();
+      else
+	board.board[i][j] = TRAP;
+      break;
+    case JOKER_REINFORCEMENT:
+      break;
+    default:
+      fputs(ERROR_YOUDONWANNANO, stderr);
+      break;
+    }
+  return 0;
+}
+
+/* used to determine when to remove a column/line (if there's no pawn on it) */
+int tile_not_empty(tile_content_t t) 
+{
+  return t == APPLE || t == CACTUS;
 }
 
 int game_play_move(move_t * move){
